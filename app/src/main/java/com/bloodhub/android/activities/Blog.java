d4e63b4c7382895573a9aeb4889df7670adb5569
karamusluk <admin@cinedia.net>
2018-04-21 18:30:58 +0300

    Blog is now more responsive

--- app/src/main/java/com/bloodhub/android/activities/Blog.java
+++ app/src/main/java/com/bloodhub/android/activities/Blog.java
@@ -2,14 +2,23 @@ package com.bloodhub.android.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebResourceRequest;
+
+import android.graphics.Bitmap;
+import android.view.View;
+import android.widget.ProgressBar;
+
+
 
 import com.bloodhub.android.R;
 
 public class Blog extends BaseActivity {
     WebView mWebView;
-
+    private ProgressBar spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +28,32 @@ public class Blog extends BaseActivity {
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         mWebView.loadUrl("http://cs491-2.mustafaculban.net/blog");
+
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return false;
+            }
+        });
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (mWebView.canGoBack()) {
+                        mWebView.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
 }
